4db435c28f11d725a5e2a2e735fec0f7963013f0
beichenstanly <zhengxgen@163.com>
2025-04-04 18:44:25 +0800

    update router

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,7 +19,20 @@
                                 cover></v-carousel-item>
                         </v-carousel>
                         <v-card-actions>
-                            <v-btn color="info" href="/photo">进入相册</v-btn>
+                            <v-btn color="info" to="/photos">进入相册</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-sheet>
+                <v-sheet border="dashed md" color="surface-light" rounded="lg" width="100%">
+                    <v-card :color="color" :variant="variant" class="mx-auto" hover prepend-icon="mdi-image-multiple">
+                        <v-card-title>
+                            深度学习项目
+                        </v-card-title>
+                        <v-card-text>
+                            基于深度学习的手写数字识别项目
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn color="info" to="/ai">进入深度学习项目</v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-sheet>
